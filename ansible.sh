12612b2e28e75afafc4fff88dc292a066bb424fb
Vivek <vivekanandareddy7677@gmail.com>
2025-02-28 11:20:52 -0600

    Updated observability tools code by using spot instances

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,2 @@
-component=$1
+component=$1 # see in pipelines
 ansible-playbook -i $component-dev.devopsjourney.fun, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e role_name=$component expense.yml
