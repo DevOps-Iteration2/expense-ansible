53ceb79b1d48d7264b1e7fb752ee8329c7a6805f
Vivek <vivekanandareddy7677@gmail.com>
2025-03-11 23:33:05 -0500

    Removing app and secrets file

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,5 @@
 component=$1 # see in pipelines
-
+rm -f ~/secrets.json ~/app.json
 ansible-playbook get-secrets.yml -e vault_token=$vault_token  -e env=$env -e role_name=$component
 ansible-playbook -i $component-$env.devopsjourney.fun, -e vault_token=$vault_token -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json' -e '@~/app.json'
 rm -f ~/secrets.json ~/app.json
