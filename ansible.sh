1fb47a4b5d871fce23b60381c431ff7f58fbbcca
Vivek <vivekanandareddy7677@gmail.com>
2025-03-28 17:07:03 -0500

    Using dynamic inventory using aws cli

--- ansible.sh
+++ ansible.sh
@@ -2,5 +2,9 @@ component=$1 # see in pipelines
 
 rm -f ~/*.json
 ansible-playbook get-secrets.yml -e vault_token=$vault_token  -e env=$env -e role_name=$component
-ansible-playbook -i $component-$env.devopsjourney.fun, -e vault_token=$vault_token -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json'
+#ansible-playbook -i $component-$env.devopsjourney.fun, -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json'
+
+aws ec2 describe-instances --filters Name=tag:Name,Values=$component-$env Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].PrivateIpAdress' --output text >inv
+
+ansible-playbook -i inv -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json'
 rm -f ~/*.json
